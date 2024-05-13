2fff3df99bb38b011b520e06aae9df591e491093
mahedi205339 <mahedi205339gmaol.com>
2024-05-13 22:27:44 +0600

    added some code in portfolio page

--- src/app/portfolio/page.jsx
+++ src/app/portfolio/page.jsx
@@ -1,21 +1,29 @@
 "use client"
-import React from 'react';
-import { motion } from 'framer-motion';
+import React, { useRef } from 'react';
+import { motion, useScroll, useTransform } from 'framer-motion';
 const PortfolioPage = () => {
+
+    const ref = useRef()
+
+    const { scrollYProgress } = useScroll()
+    const x = useTransform(scrollYProgress, [0, 1], ["0%", "100%"])
+
     return (
         <motion.div
-        className='h-full'
-        initial={{ y: "-200vh" }}
-        animate={{ y: "0%" }}
-        transition={{ duration: 1 }}
-    >
-        <div>
-            portfolio page
-            <h2>Portfolio will show here</h2>
-            <h2>Portfolio will show here</h2>
-            <h3>texting github commit if it working</h3>
-        </div>
-    </motion.div>
+            className='h-full'
+            initial={{ y: "-200vh" }}
+            animate={{ y: "0%" }}
+            transition={{ duration: 1 }}
+        >
+            <div className='h-[600vh]' ref={ref}>
+                <div className="w-screen h-[calc(100vh-6rem)] flex items-center justify-center text-8xl text-center">
+                    My Projects
+                </div>
+                <div className='sticky top-0 flex h-screen gap-4 items-center'>
+
+                </div>
+            </div>
+        </motion.div>
     );
 };
 
