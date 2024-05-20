d441c22dac29fa65a32478c0d3bdb3e33d43da7a
Mahedi205339 <mahedi205339@gmail.com>
2024-05-20 21:20:51 +0600

    added full contact page layout

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,9 +1,13 @@
 "use client"
-import React from 'react';
+import React, { useState } from 'react';
 import { motion } from 'framer-motion';
 const ContactPage = () => {
 
+    const [success, setSuccess] = useState(true)
+    const [error, setError] = useState(false)
     const text = "Say Hello";
+
+
 
 
     return (
@@ -31,17 +35,53 @@ const ContactPage = () => {
                         ))}
                         😉
                     </div>
-                    
+
                 </div>
                 {/* FORM CONTAINER */}
                 <div className=''>
-                    <form action="0">
-                        <span>Dear Mahedi Hasan,</span>
-                        <textarea name="" id=""></textarea>
-                        <span>My mail address is :</span>
-                        <input type="text" />
+                    <form className="h-1/2 lg:h-full bg-blue-100 backdrop:blur-md rounded-lg text-base flex flex-col gap-8 justify-center p-24">
+
+                        <span>Dear Mahedi,</span>
+
+                        <div className="relative z-0 my-5">
+                            <div className="flex flex-row items-center">
+                                <textarea
+                                    rows={3}
+                                    name="text"
+                                    className="resize-none peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-black focus:border-blue-600 focus:outline-none focus:ring-0"
+                                    placeholder=" "
+                                />
+
+                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform  text-neutral-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                                    Your text
+                                </label>
+                            </div>
+
+                        </div>
+                        <div className="relative z-0 my-5">
+                            <div className="flex flex-row items-center">
+                                <input
+                                    name="emails"
+                                    className="resize-none peer block w-full appearance-none border-0 border-b border-gray-500 bg-transparent py-2.5 px-0 text-black focus:border-blue-600 focus:outline-none focus:ring-0"
+                                    placeholder=" "
+                                />
+
+                                <label className="absolute top-3 -z-10 origin-[0] -translate-y-6 scale-75 transform  text-neutral-400 duration-300 peer-placeholder-shown:translate-y-0 peer-placeholder-shown:scale-100 peer-focus:left-0 peer-focus:-translate-y-6 peer-focus:scale-75 peer-focus:text-blue-600 peer-focus:dark:text-blue-500">
+                                    My email address
+                                </label>
+                            </div>
+                        </div>
+
                         <span>Regards</span>
-                        <button>Send</button>
+
+                        <div>
+                            <button className='px-3 py-2 md:px-3 md:py-3 lg:px-4 lg:py-3 text-xs lg:text-base  work-text rounded-lg ring-black bg-gradient-to-r from-cyan-500 to-blue-600 text-white mb-2'>Submit</button>
+                            <div>
+                                {success && <span className='text-green-600 font-semibold'>Your message has been sent successfully!</span>}
+                                {error && <span className='text-red-600 font font-semibold'>Something went wrong!</span>}
+                            </div>
+
+                        </div>
                     </form>
                 </div>
 
