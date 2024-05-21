869e61b119786b2054511575985357ef3eed5079
Mahedi205339 <mahedi205339@gmail.com>
2024-05-21 12:11:15 +0600

    added favicon

--- src/app/layout.js
+++ src/app/layout.js
@@ -7,6 +7,9 @@ const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
   title: "Mahedi Portfolio",
   description: "Animated portfolio page",
+  icon: {
+    icon:['/favicon.ico?v=4']
+  }
 };
 
 export default function RootLayout({ children }) {
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -92,19 +92,19 @@ const Navbar = () => {
       <div className='hidden md:flex gap-4 w-1/3 justify-end'>
         {/* TODO : input valid profile links */}
         <Link href="https://github.com/Mahedi205339">
-          <Image src="github.png" alt='github' width={24} height={24} />
+          <Image src="/github.png" alt='github' width={24} height={24} />
         </Link>
 
         <Link href="https://www.linkedin.com/in/mahedi205339/">
-          <Image src="linkedin.png" alt='linkedin' width={24} height={24} />
+          <Image src="/linkedin.png" alt='linkedin' width={24} height={24} />
         </Link>
 
         <Link href="https://www.facebook.com/profile.php?id=100077396804897">
-          <Image src="facebook.png" alt='facebook logo' width={24} height={24} />
+          <Image src="/facebook.png" alt='facebook logo' width={24} height={24} />
         </Link>
 
         <Link href="https://www.instagram.com/mahedi_hasan_turag/">
-          <Image src="instagram.png" alt='instagram' width={24} height={24} />
+          <Image src="/instagram.png" alt='instagram' width={24} height={24} />
         </Link>
 
       </div>
